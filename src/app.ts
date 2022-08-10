0f17b391270d7a7e7b2b29446b8aae81a7fb56b1
Marta Nara <marta.kos1230@gmail.com>
2022-08-10 20:46:51 +0200

    Add form rendering

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,34 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
+  hostElement: HTMLDivElement; // of HTMLElement
   element: HTMLFormElement;
+  titleInput: HTMLInputElement;
+  descriptionInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById(
-      'project-input'
-    )! as HTMLTemplateElement;
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
+    const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInput = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInput = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInput = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInput.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
